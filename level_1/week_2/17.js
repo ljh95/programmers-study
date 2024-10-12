0e249106216c70d225304589662a4b7e27162de7
jhlee <jhlee@cytur.net>
2024-10-12 18:12:21 +0900

    feat: temp

--- level_1/week_2/17.js
+++ level_1/week_2/17.js
@@ -1,4 +1,25 @@
-function solution(s, n) {
-  var answer = "";
+function solution(k, score) {
+  let min = 2000;
+  const answer = [];
+  const list = [];
+
+  for (let i = 0; i < score.length; i++) {
+    const element = score[i];
+    if (i < k) {
+      list.push(element);
+
+      min = Math.min(min, element);
+      answer.push(min);
+      continue;
+    }
+
+    list.push(element);
+    list.sort((a, b) => a - b);
+    console.log(list);
+
+    list.shift();
+
+    answer.push(list[0]);
+  }
   return answer;
 }
