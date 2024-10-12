5f4c45b5ff5c45e484b519c1ae81459128cb225d
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-10-12 18:58:24 +0900

    [level 1] Title: 추억 점수, Time: 1.57 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,14 @@
+function solution(name, yearning, photo) {
+  const m = new Map();
+  name.forEach((n, i) => {
+    m.set(n, yearning[i]);
+  });
+
+  return photo.reduce((acc, cur) => {
+    const sum = cur.reduce((a, c) => {
+      return a + (m.get(c) ?? 0);
+    }, 0);
+
+    return [...acc, sum];
+  }, []);
+}
