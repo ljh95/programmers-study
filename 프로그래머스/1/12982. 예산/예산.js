3908618f6fb907e2464ac280c19879b77e828e7f
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-10-15 08:45:43 +0900

    [level 1] Title: 예산, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,36 +1,22 @@
 function solution(d, budget) {
-  if (sum(d) === budget) return d.length;
-  if (d.length === 1) {
-    return d[0] <= budget ? 1 : 0;
-  }
-  if (d.reduce((a, c) => a + c, 0) <= budget) return d.length;
+  d.sort((a, b) => a - b);
 
-  const arr = d.sort((a, b) => a - b);
+  let left = 0;
+  let right = d.length;
 
-  const len = arr.length;
-  let i = Math.floor(len / 2);
-  let start = 0;
-  let end = len;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const sum = calculateSum(d, mid);
 
-  while (true) {
-    const prevArr = arr.slice(0, i);
-    const nextArr = arr.slice(0, i + 1);
-    console.log(i);
-
-    if (sum(prevArr) <= budget && budget <= sum(nextArr)) {
-      if (sum(nextArr) === budget) i++;
-      break;
-    } else if (sum(prevArr) < budget) {
-      start = i;
-      i = Math.floor((end + i) / 2);
+    if (sum <= budget) {
+      left = mid + 1;
     } else {
-      end = i;
-      i = Math.floor((start + i) / 2);
+      right = mid - 1;
     }
   }
-  return i;
+  return right;
 }
 
-const sum = (arr) => {
-  return arr.reduce((acc, cur) => acc + cur, 0);
-};
+function calculateSum(arr, mid) {
+  return arr.slice(0, mid).reduce((acc, cur) => acc + cur, 0);
+}
