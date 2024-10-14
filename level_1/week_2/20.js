9192481176c8cd3ecc3438b474e4f59b86f2767b
jhlee <jhlee@cytur.net>
2024-10-14 18:59:21 +0900

    feat: solve 21

--- level_1/week_2/20.js
+++ level_1/week_2/20.js
@@ -1,4 +1,13 @@
-function solution(s, n) {
-  var answer = "";
+function solution(nums) {
+  var answer = 0;
+  let halfLength = nums.length / 2;
+  let setNums = new Set(nums);
+  if (setNums.size >= halfLength) {
+    answer = halfLength;
+  } else {
+    answer = setNums.size;
+  }
+
   return answer;
 }
+//prev
