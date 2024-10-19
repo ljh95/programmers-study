c0188cb3d26dd631780daade099788b33b078d19
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-10-19 14:56:41 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,38 +1,54 @@
-const obj = {
-  S: 1,
-  D: 2,
-  T: 3,
-};
-
 function solution(dartResult) {
-  const ss = dartResult.match(/\d+\D*(?=\d|$)/g);
-  let arr = [];
-
-  for (let i = 0; i < ss.length; i++) {
-    const element = ss[i];
-    const [s_num, s_pow, grad] = element.match(/\d+|\w|\W/g);
-    const num = Number(s_num);
-    const pow = obj[s_pow];
-
-    const v = Math.pow(num, pow);
-
-    if (!grad) {
-      arr.push(v);
-      continue;
-    }
-
-    if (grad === "*") {
-      if (i > 0) {
-        arr = arr.map((a, idx) => {
-          if (idx === i - 1) return a * 2;
-          return a;
-        });
-      }
-      arr.push(v * 2);
-    } else {
-      arr.push(v * -1);
+  var answer = 0;
+  let s = dartResult;
+  let i;
+  let start = 0;
+  let arr = []
+  if (s.length == 0) {
+    return 0;
+  }
+  for (i = 2; i < s.length; i++) {
+    if (/\d/.test(s[i])) {
+      arr.push(s.slice(start, i))
+      start = i
+      i += 1
     }
   }
+  arr.push(s.slice(start, i))
+  let newArr = []
 
-  return arr.reduce((acc, cur) => acc + cur, 0);
+  arr.forEach((el, i) => {
+    let line = [];
+    line.push(el.match(/10|\d/)[0])
+    line.push(el.match(/[SDT]/)[0])
+    if (/[*#]/.test(el)) {
+      line.push(el.match(/[*#]/)[0])
+    }
+    newArr.push(line)
+  })
+  newArr.forEach((el, i) => {
+    if (el[1] === 'D') {
+      el[0] **= 2
+    } else if (el[1] === 'T') {
+      el[0] **= 3
+    }
+    el[0] *= 1
+
+    if (el[2] === '*') {
+      if (i === 0) {
+        el[0] *= 2
+      }
+      else {
+        el[0] *= 2
+        newArr[i - 1][0] *= 2
+      }
+    } else if (el[2] === '#') {
+      el[0] *= -1
+    }
+  })
+  newArr.forEach(e => {
+    answer += e[0]
+  })
+
+  return answer;
 }
