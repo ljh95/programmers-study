be7aa299f2037e7f23f373466a40bcddc8cb07a2
jhlee <jhlee@cytur.net>
2024-10-27 12:57:37 +0900

    feat: add 6, 14, 15

--- level_1/week_3/14.js
+++ level_1/week_3/14.js
@@ -1,4 +1,33 @@
-function solution(n, m, section) {
-  var answer = 0;
+function solution(board, moves) {
+  let answer = 0;
+  let arr = [];
+
+  for (let i = 0; i < moves.length; i++) {
+    const m = moves[i];
+    const [value, idx] = getValue(m, board);
+
+    if (value === undefined) continue;
+    board[idx][m - 1] = 0;
+    const prev = arr.pop();
+
+    if (prev === value) {
+      answer += 2;
+    } else {
+      arr.push(prev);
+      arr.push(value);
+    }
+  }
+
   return answer;
+}
+
+function getValue(m, board) {
+  for (let i = 0; i < board.length; i++) {
+    const element = board[i][m - 1];
+
+    if (element === 0) continue;
+    return [element, i];
+  }
+
+  return [undefined, 0];
 }
