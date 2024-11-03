100a7337265988c890851f33cba67a6da4c2178e
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-11-03 16:42:31 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,8 @@
+const reg = /[a-z]|[A-Z]/;
+
+function solution(s) {
+    return s.split(' ').map(v => {
+        if(v === '') return v;
+        return v[0].toUpperCase() + v.slice(1).toLowerCase()
+    }).join(' ')
+}
