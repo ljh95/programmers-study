68e2106a76bc297af2fc442cff1150ee8e44b4f0
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-11-03 16:45:19 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 12.06 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let count  = 0;
+    let zCount = 0;
+    
+    while(s.length > 1) {
+        count++;
+        zCount += (s.match(/0/g)||[]).length
+        s = s.replace(/0/g, '').length.toString(2);
+    }
+    
+    return [count, zCount]
+}
