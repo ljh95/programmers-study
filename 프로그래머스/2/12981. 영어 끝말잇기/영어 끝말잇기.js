a26364457d30165571b1560b325f2a634c8d8fde
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-11-10 15:40:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,19 @@
+function solution(n, words) {
+  let answer = [0, 0];
+  const set = new Set();
+
+  for (let i = 1; i < words.length; i++) {
+    set.add(words[i - 1]);
+
+    if (isWrong(words[i - 1], words[i], set)) {
+      return [(i % n) + 1, Math.floor(i / n) + 1];
+    }
+  }
+
+  return answer;
+}
+
+function isWrong(prev, cur, set) {
+  if (set.has(cur)) return true;
+  return prev[prev.length - 1] !== cur[0];
+}
