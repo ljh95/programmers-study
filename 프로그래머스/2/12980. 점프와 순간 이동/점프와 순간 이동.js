c021b899ad7265d528b3f1b5e8becbbd39ea2c9e
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-11-10 15:21:17 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let count = 1;
+  while (n > 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = n - 1;
+      count++;
+    }
+  }
+  return count;
+}
