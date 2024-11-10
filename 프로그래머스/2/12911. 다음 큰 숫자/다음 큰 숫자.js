a04417d68605c205bbe592a2ff65c13580cfb40d
ljh95 <40597662+ljh95@users.noreply.github.com>
2024-11-10 16:22:31 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.23 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,35 @@
+function solution(n) {
+    let answer = 0;
+    let small = n.toString(2);  // bin() 대신 toString(2) 사용
+    let small_count = 0;
+    
+    // 이진수에서 1의 개수 세기
+    for (let s of small) {
+        if (s === '1') {
+            small_count++;
+        }
+    }
+    
+    n++;
+    
+    while (true) {
+        let big = n.toString(2);
+        let big_count = 0;
+        
+        // 새로운 수의 이진수에서 1의 개수 세기
+        for (let s of big) {
+            if (s === '1') {
+                big_count++;
+            }
+        }
+        
+        if (big_count === small_count) {
+            answer = n;
+            break;
+        } else {
+            n++;
+        }
+    }
+    
+    return answer;
+}
